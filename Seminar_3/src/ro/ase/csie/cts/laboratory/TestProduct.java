095b6b605ec735418ba54ef2af5a769fdc4628e7
sabrinalupsan <sabrinalupsan@gmail.com>
2021-03-11 15:27:54 +0200

    added try-catch to the test class

--- Seminar_3/src/ro/ase/csie/cts/laboratory/TestProduct.java
+++ Seminar_3/src/ro/ase/csie/cts/laboratory/TestProduct.java
@@ -1,11 +1,19 @@
 package ro.ase.csie.cts.laboratory;
 
+import ro.ase.csie.cts.laboratory.exceptions.InvalidAccountAgeException;
+import ro.ase.csie.cts.laboratory.exceptions.InvalidPriceException;
+
 public class TestProduct {
 
 	public static void main(String[] args) {
 		Product product = new Product();
 		
-		product.getFinalPrice(ProductType.DISCOUNT, 0, 0);
+		try {
+			product.getFinalPrice(ProductType.DISCOUNT, 0, 0);
+		} catch (InvalidPriceException | InvalidAccountAgeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
